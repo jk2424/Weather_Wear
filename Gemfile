4ec216417c9dd8c9aab67c8bd479881bbdaf6f27
jk2424 <jkim2244@gmail.com>
2018-04-21 16:07:15 -0400

    images and logo update

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
+gem 'loading_screen', '~> 0.2.3'
+
+
 gem 'httparty'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,17 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
 //= require jquery
 //= require jquery_ujs
+//= require loading_screen
 
 $(document).on('turbolinks:load', function(){
-  $('.carousel').carousel();
+  setTimeout(function(){
+    $('.carousel').carousel();
+  }, 2000)
 });
