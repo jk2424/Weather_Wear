394e0c3d3d5175acccde6ab69fe9c68cdcb83b90
jk2424 <jkim2244@gmail.com>
2018-04-21 21:22:56 -0400

    adding api description portion

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -16,6 +16,7 @@ class PhotosController < ApplicationController
   hash = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=Miami,us&units=imperial&appid=69ee1fd6d0d4bafa14b8b23945a92179
 ").parsed_response
 @cur_temp = (hash["main"]["temp"]).to_i
+@cur_desc = hash["weather"][0]["main"]
 
 
   end
